b729033d1ed78744cd824c2a3290a20866bb6635
Hamilton Lumati <hamiltonlumati@gmail.com>
2022-02-14 18:04:07 +0100

    Validation on the creating blogs process

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -15,9 +15,10 @@ const blog_index = (req, res) => {
 const blog_update_get = (req, res) => {
     const id = req.params.id;
     const op = 1;
+    const val = 0;
     Blog.findById(id)
         .then(result => {
-            res.render('create', { op: op, blog: result, title: 'Updating a blog', id: id });
+            res.render('create', { val: val, op: op, blog: result, title: 'Updating a blog', id: id });
         })
         .catch(err => {
             console.log(err);
@@ -28,14 +29,24 @@ const blog_update_get = (req, res) => {
 const blog_update_patch = (req, res) => {
     const id = req.params.id;
     const blog = new Blog(req.body);
-
-    Blog.findByIdAndUpdate(id, { title: blog.title, snippet: blog.snippet, body: blog.body })
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    if ((blog.title.length < 4) || (blog.snippet.length < 4) || (blog.body.length < 4)) {
+        Blog.findById(id)
+            .then(result => {
+                res.render('create', { id: id, val: 1, op: 1, blog: result, title: 'Update Details' });
+            })
+            .catch(err => {
+                console.log(err);
+                res.render('404', { title: 'Blog not found' });
+            });
+    } else {
+        Blog.findByIdAndUpdate(id, { title: blog.title, snippet: blog.snippet, body: blog.body })
+            .then(result => {
+                res.redirect('/blogs');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 }
 const blog_details = (req, res) => {
     const id = req.params.id;
@@ -51,20 +62,32 @@ const blog_details = (req, res) => {
 }
 
 const blog_create_get = (req, res) => {
-    const op = 0;
 
-    res.render('create', { op: op, title: 'Create a new blog' });
+    res.render('create', { val: 0, op: 0, title: 'Create a new blog' });
 }
 
 const blog_create_post = (req, res) => {
+    const id = req.params.id;
     const blog = new Blog(req.body);
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    if ((blog.title.length < 4) || (blog.snippet.length < 4) || (blog.body.length < 4)) {
+        Blog.findById(id)
+            .then(result => {
+                res.render('create', { id: id, op: 1, val: 1, blog: result, title: 'Blog Details' });
+            })
+            .catch(err => {
+                console.log(err);
+                res.render('404', { title: 'Blog not found' });
+            });
+    } else {
+
+        blog.save()
+            .then(result => {
+                res.redirect('/blogs');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 }
 
 const blog_delete = (req, res) => {
